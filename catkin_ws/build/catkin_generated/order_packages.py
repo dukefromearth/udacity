32797f863e87a5837c2292fb2fd71859bbff3880
dukefromearth <11495234+dukefromearth@users.noreply.github.com>
2020-08-19 15:54:50 -0400

    add files

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/rex/Github/Udacity/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/rex/Github/Udacity/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/rex/Github/Udacity/catkin_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,6 +7,9 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH='/opt/ros/noetic/lib'
-export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig'
+export CMAKE_PREFIX_PATH="/home/rex/Github/Udacity/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/rex/Github/Udacity/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/rex/Github/Udacity/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/rex/Github/Udacity/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/rex/Github/Udacity/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/rex/Github/Udacity/catkin_ws/src:$ROS_PACKAGE_PATH"
